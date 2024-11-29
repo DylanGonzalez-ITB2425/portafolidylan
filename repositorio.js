fd2b40db415e52d74d105646bc0fcded5355e761
Hello World <dylan.gonzalez.7e8@itb.cat>
2024-11-29 22:45:14 +0100

    Proyecto

--- repositorio.js
+++ repositorio.js
@@ -1,4 +1,3 @@
-
 function showSection(sectionId) {
     const sections = document.querySelectorAll('.section');
     sections.forEach((section) => {
@@ -9,26 +8,16 @@ function showSection(sectionId) {
 }
 
 function copyText() {
-    // Obtén el contenido del div con id "correo_autor"
     const textElement = document.getElementById("correo_autor");
-
-    // Crea un rango y selecciona el texto
     const range = document.createRange();
     range.selectNodeContents(textElement);
-
-    // Selecciona el texto
     window.getSelection().removeAllRanges();
     window.getSelection().addRange(range);
 
-    // Intenta copiar el texto al portapapeles
     try {
         document.execCommand('copy');
-
-        // Muestra el mensaje de confirmación
         const confirmacion_mensaje = document.getElementById("confirmacion_mensaje");
         confirmacion_mensaje.style.display = "block";
-
-        // Ocultar el mensaje después de 2 segundos
         setTimeout(() => {
             confirmacion_mensaje.style.display = "none";
         }, 2000);
@@ -36,6 +25,5 @@ function copyText() {
         console.error("No se pudo copiar el texto", err);
     }
 
-    // Limpia la selección
     window.getSelection().removeAllRanges();
 }
